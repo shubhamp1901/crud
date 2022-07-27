f58f634be86d6ec43d473f8163a4cb705f6a7314
shubham <pshubham1886@gmail.com>
2022-07-28 00:32:54 +0530

    accept data function changes

--- crud-basics/app.js
+++ crud-basics/app.js
@@ -2,16 +2,24 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById('msg')
 
+let data = {}
+
 const formValidation = (e) => {
     e.preventDefault()
   if (input.value !== "") {
+    acceptData()
     console.log("success");
     msg.innerHTML = ""
   } else {
     msg.innerHTML = "Post Cannot be blank"
     console.log("failure");
-    
   }
 };
 
 form.addEventListener("submit", formValidation);
+
+const acceptData = () => {
+    data["text"] = input.value
+    console.log(data)
+}
+
