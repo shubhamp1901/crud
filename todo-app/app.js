711e3cdeefbf117772a20b54881137a3c9aac05e
shubham <pshubham1886@gmail.com>
2022-07-31 20:33:54 +0530

    localstorage changes

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,9 +4,9 @@ const dateInput = document.getElementById("dateInput");
 const textarea = document.getElementById("textarea");
 const msg = document.getElementById("msg");
 const tasks = document.getElementById("tasks");
-const add = document.getElementById('add')
+const add = document.getElementById("add");
 
-let data = {};
+let data = [{}];
 
 let formValidation = (e) => {
   e.preventDefault();
@@ -17,47 +17,72 @@ let formValidation = (e) => {
     // console.log('success')
     msg.innerHTML = "";
     acceptData();
-    add.setAttribute("data-bs-dismiss","modal")
-    add.click()
-
-    (() => {
-        add.setAttribute("data-bs-dismiss","")
-    })()
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click()(() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
   }
 };
 
 const acceptData = () => {
-  data["text"] = textInput.value;
-  data["date"] = dateInput.value;
-  data["description"] = textarea.value;
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
 
-  createTasks()
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  createTasks();
 };
 
 const createTasks = () => {
-  tasks.innerHTML += `<div>
-    <span class="fw-bold">${data.text}</span>
-    <span class="small text-secondary">${data.date}</span>
-    <p>${data.description}</p>
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+      <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+    
+            <span class="options">
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+            </span>
+          </div>
+      `);
+  });
 
-    <span class="options">
-      <i class="fas fa-edit"></i>
-      <i onClick="deleteTask(this)" class="fas fa-trash"></i>
-    </span>
-  </div>`;
-
-  resetForm()
+  resetForm();
 };
 // this means only gonna target edit or delete not entire div
 
-const resetForm  = () => {
-    textInput.value = ""
-    dateInput.value = ""
-    textarea.value = ""
-}
+const resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
 
 const deleteTask = (e) => {
-    e.parentElement.parentElement.remove()
-}
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+};
+
+const editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  console.log(data);
+  createTasks();
+})();
 
 form.addEventListener("submit", formValidation);
